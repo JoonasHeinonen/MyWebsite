a24ecdeda48c9035e87a500d2f4bd06f37807085
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2019-10-12 15:19:18 +0300

    Added social media buttons to the footer.

--- mywebsite/src/components/Footer.vue
+++ mywebsite/src/components/Footer.vue
@@ -1,6 +1,17 @@
 <template>
     <footer> 
         <h1>{{ copyright }}  {{ title }}</h1>
+        <p>Contact me on social media</p>
+        <ul>
+            <div class="someIcons" v-for="someIcon in someIcons">
+                <li>
+                    <!-- -->
+                    <a class="some-icon" v-bind:href="`${someIcon.link}`">
+                        <img class="some-icon" v-show="someIcon.show" v-bind:src="`../src/assets/some_logos/${someIcon.id}.png`">
+                    </a>
+                </li>
+            </div>
+        </ul>
     </footer> 
 </template>
 
@@ -13,9 +24,17 @@ export default {
             type: String
         }
     },
+    methods: {
+        
+    },
     data() {
         return {
-            copyright: "Copyright 2019"
+            copyright: "Copyright 2019",
+            someIcons: [
+                {id: 0, title: 'GitHub',    link: 'https://github.com/JoonasHeinonen',                      show: true}, // Github
+                {id: 1, title: 'Instagram', link: 'https://www.instagram.com/j.girdeux/',                   show: true}, // Instagram
+                {id: 2, title: 'LinkedIn',  link: 'https://www.linkedin.com/in/joonas-heinonen-32b6b414b/', show: true}, // LinkedIn
+            ]
         }
     },
     created() {
@@ -27,8 +46,33 @@ export default {
 </script>
 
 <style scoped>
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding-left: 25%;
+        padding-right: 25%;
+        overflow: hidden;
+        background-color: white;
+
+    }
+
+    li {
+        float: left;
+    }
+
+    li a {
+        color: white;
+        text-align: center;
+        text-decoration: none;
+       padding-left: 0;
+    }
+
     h1 {
         color: #0070d1;
         text-align: center;
+    }
+    .some-icon {
+         width: 20%;
+        height: 20%;
     }
 </style>
